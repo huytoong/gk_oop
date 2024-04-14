2cf37f3fc49d9dc61a35650f1c45a816b5ad0fd3
Tran Huy Tung <tranhuytunga1k51nl@gmail.com>
2024-04-14 23:01:16 +0700

    them abtract

--- bai3_c3.java
+++ bai3_c3.java
@@ -8,13 +8,91 @@ package tgk;
  *
  * @author Admin
  */
-public class bai3_c3 {
+import java.util.Scanner;
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+abstract class Polygon {
+    protected int numVertices;
+    protected double[] vertices;
+    protected double area;
+
+    public Polygon() {
+        this.numVertices = 0;
+        this.vertices = null;
+        this.area = 0;
     }
-    
+
+    public void inputVertices() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the number of vertices:");
+        this.numVertices = scanner.nextInt();
+        this.vertices = new double[this.numVertices];
+        System.out.println("Enter the vertices:");
+        for (int i = 0; i < this.numVertices; i++) {
+            this.vertices[i] = scanner.nextDouble();
+        }
+    }
+
+    public void outputVertices() {
+        System.out.println("Vertices:");
+        for (double vertex : this.vertices) {
+            System.out.println(vertex);
+        }
+    }
+
+    public abstract void calculateArea();
+}
+
+class Rectangle extends Polygon {
+    public Rectangle() {
+        super();
+    }
+
+    public void inputVertices() {
+        Scanner scanner = new Scanner(System.in);
+        this.numVertices = 2;
+        this.vertices = new double[this.numVertices];
+        System.out.println("Enter the top left and bottom right vertices:");
+        for (int i = 0; i < this.numVertices; i++) {
+            this.vertices[i] = scanner.nextDouble();
+        }
+    }
+
+    public void calculateArea() {
+        this.area = Math.abs(this.vertices[1] - this.vertices[0]);
+    }
+}
+
+class Triangle extends Polygon {
+    public Triangle() {
+        super();
+    }
+
+    public void inputVertices() {
+        Scanner scanner = new Scanner(System.in);
+        this.numVertices = 3;
+        this.vertices = new double[this.numVertices];
+        System.out.println("Enter the three vertices:");
+        for (int i = 0; i < this.numVertices; i++) {
+            this.vertices[i] = scanner.nextDouble();
+        }
+    }
+
+    public void calculateArea() {
+        double s = (this.vertices[0] + this.vertices[1] + this.vertices[2]) / 2;
+        this.area = Math.sqrt(s * (s - this.vertices[0]) * (s - this.vertices[1]) * (s - this.vertices[2]));
+    }
+}
+
+public class bai3_c3 {
+    public static void main(String[] args) {
+        Polygon rectangle = new Rectangle();
+        rectangle.inputVertices();
+        rectangle.calculateArea();
+        System.out.println("Area of rectangle: " + rectangle.area);
+
+        Polygon triangle = new Triangle();
+        triangle.inputVertices();
+        triangle.calculateArea();
+        System.out.println("Area of triangle: " + triangle.area);
+    }
 }
